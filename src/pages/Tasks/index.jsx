bd60686eece9306e4e2c286110ff65ce553e0466
@gabrielgusn <gabriel.gnicolodi@gmail.com>
2022-06-26 11:32:53 -0300

    OneDrive fix

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Title, Page, InputTaskField, InputCheckField } from './styles.js'
+import React, { useEffect, useState } from 'react';
+import { Title, Page, InputTaskField, InputCheckField, InputDeleteBtn } from './styles.js'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
-import { BsTrash } from 'react-icons/bs'
+// import { BsTrash } from 'react-icons/bs'
+import { BiTrash } from "react-icons/bi";
+import api from '../../services/api.js'
 
 function InputTask(){
     return(
@@ -12,24 +14,79 @@ function InputTask(){
     ) 
 }
 
-function InputCheck(){
+function InputCheck({ name, done, id, onUpdate, handleDeleteTask }){
+    const handleCheckTask = ()=>{
+        api.put(`/task/${id}`, {done: !done})
+        .then(()=> {
+            onUpdate()
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
     return(
-        <InputCheckField htmlFor="checkInput">
-            <BsTrash/>
-            <input type="checkbox" id="checkInput" placeholder="Add a new task" />
-            <span/>teste
-        </InputCheckField>
+        <div>
+            <InputDeleteBtn onClick={handleDeleteTask}>
+                <BiTrash/>
+            </InputDeleteBtn>
+            <InputCheckField htmlFor={id}> 
+            <input type="checkbox" id={id} placeholder="Add a new task" checked={done ? true : undefined} onChange={handleCheckTask}/>{/* defaultChecked={done} */}
+            <span/>
+            { name }
+            </InputCheckField>    
+        </div>
+        
     )
 }
 
 function Task(){
+    const [tasks, setTasks] = useState([])
+
+    const handleGetTasks = () => {
+        api.get('/task')
+        .then(response=>{
+            setTasks(response.data)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+
+    useEffect(handleGetTasks, [])
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        const { value } = event.target.elements[0]
+        await api.post('/task', {name: value}).then(
+            ()=>{
+                handleGetTasks()
+            }).catch(error=>{
+                console.log(error)
+            })
+    }
+
+    const handleDeleteTask = async (id) => { 
+        await api.delete(`/task/${id}`)
+        handleGetTasks();
+    }
+
     return(
         <Page>
             <Title>TaskDo</Title>
-            <InputTask />
-            <InputCheck />
+            <form onSubmit={handleSubmit}>
+                <InputTask />
+            </form>
+            {
+                tasks.map(task=>(
+                    <InputCheck {...task} onUpdate={handleGetTasks} handleDeleteTask={()=>{handleDeleteTask(task.id)}}
+                    />
+                ))
+            }
         </Page>
     )
 }
+
+
 
 export default Task
